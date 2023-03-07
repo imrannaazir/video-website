92fd4ac1854081d49b3e3ff0aa000abc3b2d8b64
emon <imrannaaziremon@gmail.com>
2023-03-07 22:29:04 +0600

    setup react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import VideoPage from "./pages/VideoPage";
+import Footer from "./ui/Footer";
+import Navbar from "./ui/Navbar";
+
 function App() {
   return (
-    <div >
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos/:videoId" element={<VideoPage />} />
+      </Routes>
+      <Footer />
+    </Router>
 
-    </div>
   );
 }
 
