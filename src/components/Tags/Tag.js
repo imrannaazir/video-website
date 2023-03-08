28f966262def2f9ffe70e4ff70c684aadceef586
emon <imrannaaziremon@gmail.com>
2023-03-08 11:18:55 +0600

    created tags slice

--- src/components/Tags/Tag.js
+++ src/components/Tags/Tag.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Tag() {
+export default function Tag({ tag: { title } }) {
     return (
         <div
             className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer"
         >
-            react
+            {title}
         </div>
         // {/* <!-- selected --> */}
         // <div
--- src/components/Tags/Tags.js
+++ src/components/Tags/Tags.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchTags } from '../../features/tags/tagsSlice';
+import Loading from '../../ui/Loading';
 import Tag from './Tag'
 
 export default function Tags() {
+    const dispatch = useDispatch();
+    const { tags, isLoading, isError } = useSelector(state => state.tags);
+
+    // dispatch fetched action
+    useEffect(() => {
+        dispatch(fetchTags())
+    }, [dispatch])
+
+    //render what to render in ui
+    let content;
+    if (isLoading) content = <Loading />
+    if (!isLoading && tags.length > 0) content = tags.map(tag => <Tag tag={tag} key={tag.id} />)
+    if (isError) content = null;
     return (
         <section>
             <div
                 className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 border-b overflow-y-auto"
             >
-                <Tag />
+                {content}
             </div>
         </section>
     )
